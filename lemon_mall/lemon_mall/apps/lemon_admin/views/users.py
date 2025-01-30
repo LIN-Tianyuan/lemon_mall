3dc3126eb571f26eccc85dfb3ff303742e74d74b
LIN-Tianyuan <47159399+LIN-Tianyuan@users.noreply.github.com>
2025-01-30 23:01:49 +0100

    Rewrite to get query set data

--- lemon_mall/lemon_mall/apps/lemon_admin/views/users.py
+++ lemon_mall/lemon_mall/apps/lemon_admin/views/users.py
@@ -11,3 +11,10 @@ class UserView(ListAPIView):
     serializer_class = UserSerializer
     # Pagination
     pagination_class = PageNum
+
+    # Rewrite to get query set data
+    def get_queryset(self):
+        if self.request.query_params.get('keyword') == '':
+            return User.objects.all()
+        else:
+            return User.objects.filter(username__contains=self.request.query_params.get('keyword'))
